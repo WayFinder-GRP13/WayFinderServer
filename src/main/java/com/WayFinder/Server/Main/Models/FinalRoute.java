1e2a0639fdd7ebf83754f0961cb427178370bd58
Soumen Ghosh <soumen.ghosh2205@gmail.com>
2020-02-20 15:32:49 +0000

    added class for final route

---
+++ src/main/java/com/WayFinder/Server/Main/Models/FinalRoute.java
@@ -0,0 +1,9 @@
+package com.WayFinder.Server.Main.Models;
+
+import com.WayFinder.Server.Main.NodeCreation.Node;
+
+public class FinalRoute{
+    public Node Origin;
+    public Node Destination;
+    public String OverviewPolyline;
+}
